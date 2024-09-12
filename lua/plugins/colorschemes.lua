e9bb21ce65ea6df78198fe222eae615624fa9ea6
hunzo <takraw_s@hotmail.com>
2024-09-12 08:34:11 +0700

    update colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,10 +1,16 @@
 return {
-  {
-    "Tsuzat/NeoSolarized.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd([[ colorscheme NeoSolarized ]])
-    end,
-  },
+	"craftzdog/solarized-osaka.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("solarized-osaka").setup({
+			transparent = true, -- Enable transparent background
+			terminal_colors = true, -- Use the same colors in Neovim terminal
+			styles = {
+				sidebars = "transparent", -- Transparent background for sidebars
+				floats = "transparent", -- Transparent background for floating windows
+			},
+		})
+		vim.cmd([[colorscheme solarized-osaka]])
+	end,
 }
